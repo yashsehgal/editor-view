64e47e45343d836dd5bfe65b83e541beff8cb92a
Yash Sehgal <yash.sehgal@rocketium.com>
2024-04-06 22:02:51 +0530

    feat: support for folder creation via folder action

--- src/modules/ProjectStructureTree/ProjectStructureTree.tsx
+++ src/modules/ProjectStructureTree/ProjectStructureTree.tsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { CollapsibleFeatureSection } from "../../components/CollapsibleFeatureSection";
 import { ProjectStructureContext } from "../../contexts/ProjectStructureContext";
 import { ProjectFolderNode } from "../../components/ProjectFolderNode";
-import { IconFilePlus } from "@tabler/icons-react";
+import { IconFilePlus, IconFolderPlus, IconFolder } from "@tabler/icons-react";
 import { cn } from "../../dev-utils/cn";
 import { FileIconType, getFileTypeIcon } from "../helpers/getFileTypeIcon";
 import { motion } from "framer-motion";
@@ -12,6 +12,8 @@ export function ProjectStructureTree() {
     ProjectStructureContext
   );
   const [showCreateNewFile, setShowCreateNewFile] = useState<boolean>(false);
+  const [showCreateNewFolder, setShowCreateNewFolder] =
+    useState<boolean>(false);
 
   const handleCreateNewFile = (newFileName: string) => {
     setShowCreateNewFile(false);
@@ -45,12 +47,43 @@ export function ProjectStructureTree() {
     }
   };
 
+  const handleCreateNewFolder = (newFolderName: string) => {
+    setShowCreateNewFolder(false);
+
+    if (!newFolderName) return;
+
+    let updatedProjectStructure = [...projectStructure];
+    const pathSegments = newFolderName.split("/");
+    const fileName = pathSegments.pop();
+    let currentFolder: { name: string; inner?: { name: string }[] }[] =
+      updatedProjectStructure;
+    for (const folderName of pathSegments) {
+      const folderIndex = currentFolder.findIndex(
+        (item) => item.name === folderName
+      );
+      if (folderIndex > -1) {
+        currentFolder = currentFolder[folderIndex].inner || [];
+      } else {
+        const newFolder = { name: folderName, inner: [] };
+        currentFolder.push(newFolder);
+        currentFolder = newFolder.inner;
+      }
+    }
+    currentFolder.push({ name: fileName!, inner: [] });
+    setProjectStructure(updatedProjectStructure);
+  };
+
   return (
     <CollapsibleFeatureSection
       sectionTitle={projectName}
       actions={[
         {
-          name: "Create New File",
+          name: "Create new folder",
+          onClick: () => setShowCreateNewFolder(true),
+          icon: <IconFolderPlus size={14} />,
+        },
+        {
+          name: "Create new file",
           onClick: () => setShowCreateNewFile(true),
           icon: <IconFilePlus size={14} />,
         },
@@ -62,7 +95,48 @@ export function ProjectStructureTree() {
       ) : (
         <></>
       )}
+      {showCreateNewFolder ? (
+        <CreateNewFolderInput handleCreateNewFolder={handleCreateNewFolder} />
+      ) : (
+        <></>
+      )}
     </CollapsibleFeatureSection>
+  );
+}
+
+function CreateNewFolderInput({
+  handleCreateNewFolder,
+}: {
+  handleCreateNewFolder: (newFolderName: string) => void;
+}) {
+  const [newFolderName, setNewFolderName] = useState<string>("");
+
+  return (
+    <div
+      className={cn(
+        "ProjectFolderNode-container text-sm font-medium flex flex-row gap-1.5 items-center w-full px-2.5"
+      )}
+    >
+      <span className="ProjectFolderNode-iconContainer">
+        <IconFolder size={14} />
+      </span>
+      <input
+        type="text"
+        className="CreateNewFileInput-input py-0.5 px-1 bg-transparent w-full tracking-tighter"
+        onChange={(e) => {
+          setNewFolderName(e.target.value as string);
+        }}
+        onBlur={(e) => {
+          handleCreateNewFolder(newFolderName);
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleCreateNewFolder(newFolderName);
+          }
+        }}
+        autoFocus
+      />
+    </div>
   );
 }
 
@@ -93,8 +167,12 @@ function CreateNewFileInput({
       <input
         type="text"
         className="CreateNewFileInput-input py-0.5 px-1 bg-transparent w-full tracking-tighter"
-        onChange={(e) => setNewFileName(e.target.value as string)}
-        onBlur={() => handleCreateNewFile(newFileName)}
+        onChange={(e) => {
+          setNewFileName(e.target.value as string);
+        }}
+        onBlur={(e) => {
+          handleCreateNewFile(newFileName);
+        }}
         onKeyDown={(e) => {
           if (e.key === "Enter") {
             handleCreateNewFile(newFileName);
